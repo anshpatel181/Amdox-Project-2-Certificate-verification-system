f5a5b6cb499a7dbe1b079377e506e9a8f7cf1b3e
ansh46033512 <patelansh6989@gmail.com>
2026-03-31 10:35:40 +0530

    solved error

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import authRoutes from "./routes/authRoutes.js"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
 dotenv.config();
 connectDB();
