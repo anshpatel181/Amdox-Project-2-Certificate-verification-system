9622348eefe065918b7e46396b3575345e1717bd
ansh46033512 <patelansh6989@gmail.com>
2026-03-31 11:50:38 +0530

    testing webhook

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,6 +1,22 @@
-export const clerkWebhooks = (req, res) => {
+import { Webhook } from "svix"
+
+export const clerkWebhooks = async (req, res) => {
     try {   
-        res.send("This is webhook page");
+        
+        console.log("Webhook received!");
+        // create a webhook svix instance
+        const webhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        await webhook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        });
+
+        const {data, type} = req.body;
+        console.log("Data: ", data);
+        console.log("type: ", type);
+        
     } catch (error) {
         
     }
